424a7da67d728508dde64f5a8b4093dbabedb2ce
SmavaProject <katyakravchenka@yahoo.com>
2021-03-09 10:02:43 -0800

    ClimbStairs

---
+++ src/problems/easy/ClimbStairs.java
@@ -0,0 +1,38 @@
+package problems.easy;
+
+/***
+ * https://leetcode.com/problems/climbing-stairs/  #70 EASY
+ * Dynamic programming
+ * Важдно распознать зависимость на стр 25 как в Фибоначи
+ */
+public class ClimbStairs
+{
+    public int climbStairs(int n) {
+        if(n == 0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        if(n==2){
+            return 2;
+        }
+        int[] numberOfStairs = new int[n+1];
+        numberOfStairs[0] = 0;
+        numberOfStairs[1] = 1;
+        numberOfStairs[2] = 2;
+        for (int i = 3; i<= n; i++){
+            numberOfStairs[i] =numberOfStairs[i-1] + numberOfStairs[i-2];
+        }
+        return numberOfStairs[n];
+    }
+}
+
+
+/***
+ * n=1 => 1 (1)
+ * n=2 => 2 (1+1, 2)
+ * n=3 => 3 (1+2, 2+1, 1+1+1)
+ * n=4 => 5 (2+2, 1+2+1, 1+1+2, 2+1+1, 1+1+1+1)
+ */
+
