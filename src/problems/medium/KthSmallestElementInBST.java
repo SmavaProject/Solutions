972012a4756fd4547ff69650230e14b2e17093e1
SmavaProject <katyakravchenka@yahoo.com>
2022-01-22 08:04:27 -0800

    KthSmallestElementInBSTIterative

--- src/problems/medium/KthSmallestElementInBST.java
+++ src/problems/medium/KthSmallestElementInBST.java
@@ -1,4 +1,31 @@
 package problems.medium;
 
+import java.util.ArrayList;
+
 public class KthSmallestElementInBST {
+    /***
+     * #230 MEDIUM
+        https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+     */
+
+
+    public int kthSmallest(TreeNode root, int k) {
+
+        ArrayList<Integer> elements = new ArrayList<>();
+        inorder(root, elements, k);
+        return elements.get(k-1);
+    }
+
+    public void inorder(TreeNode root, ArrayList<Integer> elements, int k){
+        if(root == null){
+            return;
+        }
+        inorder(root.left, elements, k);
+        elements.add(root.val);
+        //optimization
+        if(elements.size() == k){
+            return;
+        }
+        inorder(root.right, elements, k);
+    }
 }
---
+++ src/problems/medium/KthSmallestElementInBSTIterative.java
@@ -0,0 +1,42 @@
+package problems.medium;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class KthSmallestElementInBSTIterative {
+    /***
+     * #230 MEDIUM
+     * Iterative solution
+     https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+     */
+
+    public int kthSmallest(TreeNode root, int k) {
+        if(root == null){
+            return 0;
+        }
+
+        ArrayList<Integer> elements = new ArrayList<>();
+
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        TreeNode current = root.left;
+        while(stack.size()>0 || current!=null){
+            //Step1: add all current.left nodes to the stack
+            if(current!=null){
+                stack.push(current);
+                current = current.left;
+            }else{
+                //Step 2: when there are no current.left nodes, start processing node from the stack
+                TreeNode node = stack.pop();
+                elements.add(node.val);
+                //Step 3: go to the right subtree.
+                current = node.right;
+                //Return to Step 1 and repeat
+            }
+
+
+        }
+        return elements.get(k-1)!=null ? elements.get(k-1) : 0;
+
+    }
+}
