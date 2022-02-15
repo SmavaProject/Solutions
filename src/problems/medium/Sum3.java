23bcdb90ae7e43ad84527ecedc86f36f9af6e43b
SmavaProject <katyakravchenka@yahoo.com>
2022-02-15 08:31:54 -0800

    3Sum

---
+++ src/problems/medium/Sum3.java
@@ -0,0 +1,54 @@
+package problems.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Sum3 {
+    /***
+     * # 15. 3Sum
+     * https://leetcode.com/problems/3sum/
+     * Triplet Sum to Zero (medium):
+     * https://www.educative.io/courses/grokking-the-coding-interview/gxk639mrr5r
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>>  result = new ArrayList<>();
+        if(nums.length<3){
+            return result;
+        }
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i< nums.length; i++){
+            if(i==0 || nums[i]!=nums[i-1]){ // <--- avoid duplicates!
+                searchPairs(nums, -nums[i], i+1, result);
+            }
+        }
+
+        return result;
+    }
+
+    public void searchPairs(int[] nums, int targetSum, int leftPointer, List<List<Integer>> result){
+        int rightPointer = nums.length-1;
+        while(leftPointer<rightPointer){
+            if(nums[leftPointer] + nums[rightPointer] == targetSum){
+                result.add(Arrays.asList(-targetSum, nums[leftPointer], nums[rightPointer]));
+
+                rightPointer--;
+                leftPointer++;
+
+                while(rightPointer>leftPointer && nums[rightPointer]==nums[rightPointer+1]){ // <--- avoid duplicates!
+                    rightPointer--;
+                }
+                while(leftPointer<rightPointer && nums[leftPointer]==nums[leftPointer-1]){ // <--- avoid duplicates!
+                    leftPointer++;
+                }
+
+            }else if(nums[leftPointer] + nums[rightPointer] > targetSum){
+                rightPointer--;
+            }else{
+                leftPointer++;
+            }
+        }
+    }
+}
