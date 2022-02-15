7abcd85cea6585f5d220f6e3f4723a3f48bc3859
SmavaProject <katyakravchenka@yahoo.com>
2022-02-15 07:33:43 -0800

    SortCharactersByFrequency

---
+++ src/problems/medium/SortCharactersByFrequency.java
@@ -0,0 +1,39 @@
+package problems.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class SortCharactersByFrequency {
+
+    /***
+     * #451. Sort Characters By Frequency - MEDIUM
+     * https://leetcode.com/problems/sort-characters-by-frequency/
+     *
+     * https://www.educative.io/courses/grokking-the-coding-interview/gxZz615BPPG
+     *
+     */
+
+    public String frequencySort(String s) {
+        HashMap<Character, Integer> frequencyMap = new HashMap<>();
+        for (Character c: s.toCharArray()){
+            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0)+1);
+        }
+        //elements are sorted in descending order (by frequncy)
+        //maxHeap: [e=2, t=1, r=1]
+        PriorityQueue<Map.Entry<Character,Integer>> maxHeap = new PriorityQueue<>((c1, c2) -> c2.getValue() - c1.getValue());
+
+        maxHeap.addAll(frequencyMap.entrySet());
+
+        StringBuilder sb = new StringBuilder();
+
+        while(!maxHeap.isEmpty()){
+            Map.Entry<Character, Integer> ent = maxHeap.poll();
+            for(int i = 0; i< ent.getValue(); i++){
+                sb.append(ent.getKey());
+            }
+        }
+        return sb.toString();
+
+    }
+}
