74143bd2df018262edbacdd0f7618bbefe5340cc
SmavaProject <katyakravchenka@yahoo.com>
2022-04-18 10:34:58 -0700

    KthSmallestElementInBST

--- src/problems/medium/KthSmallestElementInBST.java
+++ src/problems/medium/KthSmallestElementInBST.java
@@ -1,6 +1,7 @@
 package problems.medium;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class KthSmallestElementInBST {
     /***
@@ -27,5 +28,32 @@ public class KthSmallestElementInBST {
             return;
         }
         inorder(root.right, elements, k);
+    }
+
+
+    /*
+    Iterative solution:
+     */
+    public int kthSmallestIterative(TreeNode root, int k) {
+        if(root == null){
+            return 0;
+        }
+
+        ArrayList<Integer> elements = new ArrayList<>();
+
+        LinkedList<TreeNode> stack = new LinkedList<>();
+
+        while(true){
+            while(root!=null){
+                stack.push(root);
+                root = root.left;
+            }
+            TreeNode c = stack.pop();
+            elements.add(c.val);
+            root = c.right;
+            if(stack.isEmpty() && root==null)break;
+        }
+        return elements.size()>=k ? elements.get(k-1) : 0;
+
     }
 }
