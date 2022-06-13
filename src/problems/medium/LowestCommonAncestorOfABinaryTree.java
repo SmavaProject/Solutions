ef7a38e54ea0984e4d2ed2751eaf6f62b8bb0c40
SmavaProject <katyakravchenka@yahoo.com>
2022-06-12 20:43:54 -0700

    LowestCommonAncestorOfABinaryTree

--- src/problems/medium/LowestCommonAncestorOfABinaryTree.java
+++ src/problems/medium/LowestCommonAncestorOfABinaryTree.java
@@ -1,5 +1,10 @@
 package problems.medium;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
 /***
  * POST Order traversal
  * Мы traverse дерево POST Order. Могут быть 2 случая:
@@ -40,4 +45,52 @@ public class LowestCommonAncestorOfABinaryTree
         //if none of the above is the case -> there is no LCA
         return null;
     }
+
+    ///// SOLUTION #2
+
+
+    /*
+     * Траверсим все дерево TreeNode root до тех пор пока не найдем nodes p, q
+     * Находим всех Ancestors для node p и помещаем их в сет
+     * Начинаем искать всех Ancestors для node q и проверять не содержится ли какой-то в Ancestors set for node p
+     * Если да, то это и есть lowest common
+     */
+    class Solution {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            HashMap<TreeNode, TreeNode> parents = new HashMap<>();
+            parents.put(root, null);
+
+            Stack<TreeNode> stack = new Stack<>();
+            stack.push(root);
+
+            while(!parents.containsKey(p) || !parents.containsKey(q)){
+
+                TreeNode curr = stack.pop();
+                if(curr.left!=null){
+                    parents.put(curr.left, curr);
+                    stack.push(curr.left);
+                }
+                if(curr.right!=null){
+                    parents.put(curr.right, curr);
+                    stack.push(curr.right);
+                }
+
+            }
+
+            // Ancestors set for node p
+            Set<TreeNode> parentsSet = new HashSet<>();
+            while(p!=null){
+                parentsSet.add(p);
+                p = parents.get(p);
+            }
+
+            //the first ancestor of q which we find in parentsSet of p is the lowest common
+            while(!parentsSet.contains(q)){
+                q = parents.get(q);
+            }
+
+            return q;
+        }
+    }
 }
+
