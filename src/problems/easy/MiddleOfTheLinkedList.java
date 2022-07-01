c812937b2db100590c472b2a8d946ec124e41676
SmavaProject <katyakravchenka@yahoo.com>
2022-06-30 19:30:39 -0700

    MiddleOfTheLinkedList

--- src/problems/easy/MiddleOfTheLinkedList.java
+++ src/problems/easy/MiddleOfTheLinkedList.java
@@ -5,6 +5,23 @@ public class MiddleOfTheLinkedList {
      * #876. Middle of the Linked List
      * https://leetcode.com/problems/middle-of-the-linked-list/
      */
+
+    //#1 - fast and slow pointer
+    public ListNode middleNode1(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast!=null && fast.next!=null){ //когда fast доберется до конца slow будет как раз на середине
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+
+
+    // #2
     public ListNode middleNode(ListNode head) {
         int len = 0;
         ListNode node = head;
